53209130538a8a20acf035b91699faeaea77671a
vsqzz <kaiontop21@icloud.com>
2024-11-23 16:38:17 +0100

    Update page.tsx

--- src/app/purchase/completed/page.tsx
+++ src/app/purchase/completed/page.tsx
@@ -32,7 +32,7 @@ export default async function Page({
 
     if (serials.length === 1) {
         const serial = serials[0];
-        const { rows } = await sql`SELECT * FROM Vanityﾒ𝟶_keys WHERE serial = ${serial}`;
+        const { rows } = await sql`SELECT * FROM mspaint_keys WHERE serial = ${serial}`;
     
         if (rows.length === 0 || rows[0].claimed === true) {
             return notFound();
@@ -42,9 +42,9 @@ export default async function Page({
             <main className="flex justify-center items-center flex-col h-screen">
                 <Card className="max-w-[475px]">
                     <CardHeader>
-                        <CardTitle>Vanityﾒ𝟶 key successful!</CardTitle>
+                        <CardTitle>mspaint key purchase successful!</CardTitle>
                         <CardDescription>
-                            Thank you for your support!
+                            Thank you for your support! Only share this link if you are gifting the key to someone else.
                         </CardDescription>
                     </CardHeader>
                     <CardContent>
@@ -92,6 +92,29 @@ export default async function Page({
                 <p className="text-sm mt-2">
                     <span className="text-muted-foreground">Order ID:</span> {rows[0].order_id}
                 </p>
+            </main>
+        )
+    } else {
+        return (
+            <main className="flex justify-center items-center flex-col h-screen">
+                <Card className="max-w-[475px]">
+                    <CardHeader>
+                        <CardTitle>mspaint bulk keys purchase successful!</CardTitle>
+                        <CardDescription>
+                            You have purchased multiple mspaint serials, to redeem them open the links below:
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-col mt-2 gap-2">
+                            {serials.map((serial) => (
+                                <div className="flex flex-row items-center gap-2" key={serial}>
+                                    <Input readOnly value={process.env.NODE_ENV === "development" ? `http://localhost:3000/purchase/completed?serial=${serial}` : `https://www.mspaint.cc/purchase/completed?serial=${serial}`}/>
+                                    <CopyButton text={process.env.NODE_ENV === "development" ? `http://localhost:3000/purchase/completed?serial=${serial}` : `https://www.mspaint.cc/purchase/completed?serial=${serial}`} />
+                                </div>
+                            ))}
+                        </div>
+                    </CardContent>
+                </Card>
             </main>
         )
     }
